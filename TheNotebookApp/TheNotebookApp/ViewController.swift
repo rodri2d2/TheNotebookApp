c1d7d1d985c03d773ae7306eddc0014a0768c41b
Rodrigo Candido <rodrigoipu@hotmail.com>
2021-02-04 17:40:46 +0100

    Initial Commit - create empty project

---
+++ TheNotebookApp/TheNotebookApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheNotebookApp
+//
+//  Created by Rodrigo Candido on 4/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
